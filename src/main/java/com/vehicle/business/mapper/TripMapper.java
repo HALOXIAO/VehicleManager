3a66c5533444fd5fca91da45429c05c5d8300f97
HALOXIAO <584714929@qq.com>
2020-06-18 09:07:05 +0800

    TripMapper#updateTrip debug

--- src/main/java/com/vehicle/business/mapper/TripMapper.java
+++ src/main/java/com/vehicle/business/mapper/TripMapper.java
@@ -46,17 +46,34 @@ public class TripMapper {
     public boolean updateTrip(Trip trip, @NotNull Session session) {
         StringBuilder updateQuery = new StringBuilder("UPDATE Driver SET ");
         Optional<LocalDateTime> dateOptional = Optional.ofNullable(trip.getDate());
-        dateOptional.ifPresent((p) -> updateQuery.append(" date= ").append(p.format(DateTimeUtils.TIME_FORMATTER)));
+        dateOptional.ifPresent((p) -> updateQuery.append(" date=?1  ,"));
         Optional<Integer> routeIdOptional = Optional.ofNullable(trip.getRouteId());
-        routeIdOptional.ifPresent((p) -> updateQuery.append(" routeId= ").append(p));
+        routeIdOptional.ifPresent((p) -> updateQuery.append(" routeId=?2  ,"));
         Optional<Integer> seatOptional = Optional.ofNullable(trip.getSeats());
-        seatOptional.ifPresent((p) -> updateQuery.append(" seats= ").append(p));
+        seatOptional.ifPresent((p) -> updateQuery.append(" seats=?3  , "));
         Optional<String> vehicleNumberOptional = Optional.ofNullable(trip.getVehicleNumber());
-        vehicleNumberOptional.ifPresent((p) -> updateQuery.append(" vehicleNumber= ").append(p));
+        vehicleNumberOptional.ifPresent((p) -> updateQuery.append(" vehicleNumber=?4  ,"));
         Optional<Integer> statusOptional = Optional.ofNullable(trip.getStatus());
-        statusOptional.ifPresent((p) -> updateQuery.append(" status= ").append(trip.getStatus()));
-        updateQuery.append(" WHERE id= ").append(trip.getId());
+        statusOptional.ifPresent((p) -> updateQuery.append(" status=?5  , "));
+        updateQuery.deleteCharAt(updateQuery.lastIndexOf(","));
+        updateQuery.append(" WHERE id=?6   ").append(trip.getId());
         Query query = session.createQuery(updateQuery.toString());
+        if (dateOptional.isPresent()) {
+            query.setParameter(1, trip.getDate().format(DateTimeUtils.TIME_FORMATTER));
+        }
+        if (routeIdOptional.isPresent()) {
+            query.setParameter(2, trip.getRouteId());
+        }
+        if(seatOptional.isPresent()){
+            query.setParameter(3, trip.getSeats());
+        }
+        if(vehicleNumberOptional.isPresent()){
+            query.setParameter(4, trip.getVehicleNumber());
+        }
+        if(statusOptional.isPresent()){
+            query.setParameter(5, trip.getStatus());
+        }
+        query.setParameter(6, trip.getId());
         int row = query.executeUpdate();
         return row == 1;
     }
