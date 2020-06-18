12d621dd36c0cda3da4fc0a7cac6ec0495e9cada
HALOXIAO <584714929@qq.com>
2020-06-18 09:31:06 +0800

    init aop

---
+++ src/main/java/com/vehicle/framework/aop/advice/MethodBeforeAdvice.java
@@ -0,0 +1,8 @@
+package com.vehicle.framework.aop.advice;
+
+/**
+ * @author HALOXIAO
+ * @since 2020-06-18 09:29
+ **/
+public interface MethodBeforeAdvice extends Advice {
+}
---
+++ src/main/java/com/vehicle/framework/aop/advice/ThrowsAdvice.java
@@ -0,0 +1,8 @@
+package com.vehicle.framework.aop.advice;
+
+/**
+ * @author HALOXIAO
+ * @since 2020-06-18 09:30
+ **/
+public interface ThrowsAdvice extends Advice {
+}
